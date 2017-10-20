fb7d78172df034b3fd5430dc67503405a6097fda
Alison Lawyer <alison@Alisons-MacBook-Air.local>
2017-10-20 08:00:16 -0700

    questions added, app functional

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,24 @@
 
 var friendArray = [
   {
-    name: 'Pam',
-    image: 'http://images2.fanpop.com/images/photos/5400000/Promo-Photo-pam-beesly-5491417-330-495.jpg',
+    name: 'Pam Beesly',
+    image: 'https://upload.wikimedia.org/wikipedia/en/6/67/Pam_Beesley.jpg',
     score: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
   },
   {
-    name: 'Jim',
+    name: 'Jim Halpert',
     image: 'https://vignette.wikia.nocookie.net/theoffice/images/9/9a/Jim.jpg/revision/latest/scale-to-width-down/350?cb=20170701084550',
     score: [1, 3, 2, 5, 4, 3, 2, 1, 5, 4]
+  },
+  {
+    name: 'Michael Scott',
+    image: 'https://vignette.wikia.nocookie.net/theoffice/images/0/02/Michael_Scott.jpg/revision/latest/scale-to-width-down/350?cb=20170701090332',
+    score: [5, 3, 1, 2, 4, 3, 5, 2, 1, 4]
+  },
+  {
+    name: 'Dwight Shrute',
+    image: 'https://vignette.wikia.nocookie.net/theoffice/images/c/c5/Dwight_.jpg/revision/latest/scale-to-width-down/350?cb=20170701082424',
+    score: [3, 4, 1, 2, 5, 3, 1, 4, 5, 2]
   }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
 
   app.post('/api/friends', function (req, res) {
     friendData.push(req.body);
-    // req.body returns an object of the user's answers (i.e. what we send over in post)
+
     var curScore = req.body.score;
     var bestFriend = 0;
     var lowestScore = 40;
@@ -16,20 +16,15 @@ module.exports = function (app) {
     for (var i = 0; i < (friendData.length - 1); i++) {
       var compScore = friendData[i].score;
       var totalDifference = 0;
-      console.log('compscore:', compScore);
 
       for (var j = 0; j < curScore.length; j++) {
-        var singleDifference = Math.abs(parseInt(curScore[j]) - compScore[j]);
+        var singleDifference = Math.abs(parseInt(curScore[j]) - parseInt(compScore[j]));
         totalDifference = totalDifference + singleDifference;
+      }
 
-        if (totalDifference < lowestScore) {
-          lowestScore = totalDifference;
-          bestFriend = i;
-
-          console.log('totalDifference:', totalDifference);
-          console.log('lowestScore:', lowestScore);
-          console.log('bestFriend:', bestFriend);
-        }
+      if (totalDifference < lowestScore) {
+        lowestScore = totalDifference;
+        bestFriend = i;
       }
     }
 
